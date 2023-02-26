b83d120896f21ae1439a21ec564fbfbcf8dbe4d4
Marco Guzman <marco_guzman@epam.com>
2023-02-26 13:26:18 -0600

    Added productions Lambda functions - With TS errors

--- src/functions/getProductsList/handler.ts
+++ src/functions/getProductsList/handler.ts
@@ -4,11 +4,12 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
-const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+import products from '@mock/products.json';
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
   return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
+    products
   });
 };
 
-export const main = middyfy(hello);
+export const main = middyfy(getProductsList);
